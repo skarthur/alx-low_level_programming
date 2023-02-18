1034fedc82c692bf0dbcc829a8bd553c965b46ce
Samuel Arthur <arthursk32@gmail.com>
2023-02-17 17:32:19 -0800

    Using variables, conditionals and loops

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		printf("%d", number);
+		number++;
+	}
+	printf("\n");
+	return (0);
+}
