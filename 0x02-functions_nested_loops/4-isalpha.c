e63c5d4549fac1550fe16b991e11b23f130d91c0
Samuel Arthur <arthursk32@gmail.com>
2023-02-21 15:45:50 -0800

    Task 4.0

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character input
+ * Return: 1 if c is a letter, 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}
