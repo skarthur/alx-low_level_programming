a6fad446cde228ef3d3c5f27d48a338831fb176a
Samuel Arthur <arthursk32@gmail.com>
2023-02-22 07:52:44 -0800

    Task 16.0

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 Fibonacci numbers, starting with
+ * 1 and 2, separated by a comma followed by a space
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned long f1 = 0, f2 = 1, sum;
+	int count = 0;
+
+	while (count < 98)
+	{
+		sum = f1 + f2;
+		printf("%lu, ", sum);
+
+		f1 = f2;
+		f2 = sum;
+
+		count++;
+	}
+	printf("\n");
+
+	return (0);
+}
