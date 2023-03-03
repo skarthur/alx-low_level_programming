f4eadbf6a5ab8d4efa2fec6646a3f545be5b63e7
Samuel Arthur <arthursk32@gmail.com>
2023-03-02 16:43:00 -0800

    Corrections

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -17,7 +17,7 @@ char *rot13(char *s)
 		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
 		{
 			s[i] = (s[i] - 65 > 25) ?
-			storel[s[i] - 97] : storeh[s[i] - 65];
+				storel[s[i] - 97] : storeh[s[i] - 65];
 		}
 	}
 	return (s);
