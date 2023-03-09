dbd9344a0e417e0a988017bd57d760ae97af3b51
Samuel Arthur <arthursk32@gmail.com>
2023-03-09 04:47:47 -0800

    Compare two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,61 +1,23 @@
-#include "main.h"
-
 /**
- * wildcmp - compare two strings with "wildcard expansion" capabilities
- * @s1: string 1
- * @s2: string 2
- * Return: 1 if strings can be considered identical, else 0
+ * wildcmp - Compare two strings allowing for wildcard char
+ * @s1: String being compared
+ * @s2: String being compared against
+ *
+ * Return: 1 if considered identical, 0 otherwise
  */
 
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	else if (*s1 == '\0' || *s2 == '\0')
-	{
-		if (*s1 == '\0' && *s2 == '*')
-			return (wildcmp(s1, ++s2));
-		else if (*s1 == '*' && *s2 == '\0')
-			return (wildcmp(++s1, s2));
-		return (0);
-	}
-
 	if (*s1 == *s2)
-		return (wildcmp(++s1, ++s2));
-	else if (*s1 == '*')
-	{
-		if (*(s1 + 1) == '*')
-			return (wildcmp(++s1, s2));
-		else
-			return (wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2));
-	}
-	else if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
 	{
 		if (*(s2 + 1) == '*')
-			return (wildcmp(s1, ++s2));
-		else
-			return (wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2));
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
 	}
-
 	return (0);
-
-}
-
-/**
- * findsrc - locate string
- * @s: string
- * @c: character
- * @i: integer value
- * @p: integer value
- * Return: values
- */
-
-int findsrc(char *s, char c, int i, int p)
-{
-	if (*(s + i) == '\0')
-		return (p + 1);
-	else if (*(s + i) == c || *(s + i) == '*')
-		p = i;
-
-	return (findsrc(s, c, i + 1, p));
 }
