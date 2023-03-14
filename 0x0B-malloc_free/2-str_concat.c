e785f35e991a794b2e15a16e234300f241b037de
Samuel Arthur <arthursk32@gmail.com>
2023-03-14 16:04:09 -0700

    Create arrays

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * str_concat - Get strings and join them
+ * str_concat - Joins two strings
  * @s1: first string
  * @s2: second string
  * Return: s1 + s2
@@ -11,34 +11,28 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int str1, str2, i = 0;
+	unsigned int i = 0, j = 0, k;
 	char *array;
 
 	if (s1 == NULL || s2 == NULL)
 		s1 = s2 = "";
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
 
-	for (str1 = 0; str1 <= *s1; str1++)
-		;
-	for (str2 = 0; str2 <= *s2; str2++)
-		;
-
-	array = malloc(sizeof(char) * (str1 + str2 + 1));
-
+	array = malloc((i + j + 1) * sizeof(char));
 	if (array == NULL)
 		return (NULL);
 
-	while (*s1)
+	for (k = 0; k < i; k++)
+		array[k] = s1[k];
+	i = k;
+	for (k = 0; k < j; k++)
 	{
-		array[i] = *s1;
+		array[i] = s2[k];
 		i++;
-		s1++;
 	}
-
-	while (*s2)
-	{
-		array[i] = *s2;
-		i++;
-		s2++;
-	}
+	array[i] = '\0';
 	return (array);
 }
