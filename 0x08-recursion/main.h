d584725d1303fc49ce326292a8c177f032d9d78c
skarthur <arthursk32@gmail.com>
2023-06-07 14:36:01 -0700

    Recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,31 @@
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int halp(int c, int i);
+
+int _sqrt_recursion(int n);
+
+int _sqrt(int x, int i);
+
+int hai(int n, int d);
+
+int is_prime_number(int n);
+
+int get_length(char *s);
+
+int my_pal(char *s, int l);
+
+int is_palindrome(char *s);
+
+int findsrc(char *s, char c, int i, int p);
+
+int wildcmp(char *s1, char *s2);
