8f78dc008acfe05dec3f668f4bd9d37eb5102e49
skarthur <arthursk32@gmail.com>
2024-01-18 12:17:00 -0800

    malloc & free

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two positive numbers.
+ * @argc: The size of the argv array.
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: 0 or 1
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		exit(98);
+	}
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
